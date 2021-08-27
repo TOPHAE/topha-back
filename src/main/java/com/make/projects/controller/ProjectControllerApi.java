b4c121343ceae7c110515ad8b4ab69a3b8e13fb4
woojin126 <woojin126@naver.com>
2021-08-27 20:37:18 +0900

    dummy

--- src/main/java/com/make/projects/controller/ProjectControllerApi.java
+++ src/main/java/com/make/projects/controller/ProjectControllerApi.java
@@ -5,13 +5,11 @@ import com.make.projects.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class ProjectControllerApi {
