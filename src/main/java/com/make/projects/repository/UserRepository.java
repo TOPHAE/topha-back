c45cb7275a81cc8a51d549d2eaadb502ebc7bda1
woojin126 <woojin126@naver.com>
2021-08-29 20:56:48 +0900

    UserRepository

---
+++ src/main/java/com/make/projects/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.make.projects.repository;
+
+import com.make.projects.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users,Long> {
+    Optional<Users> findByEmail(String email);
+}
