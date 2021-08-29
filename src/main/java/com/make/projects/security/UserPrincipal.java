1dd7d26e376196bf6b7730abe68e9cad853c6024
woojin126 <woojin126@naver.com>
2021-08-29 20:58:25 +0900

    UserPrincipal 스프링 시큐리티 VO (인증된 사용자정보 저장)

---
+++ src/main/java/com/make/projects/security/UserPrincipal.java
@@ -0,0 +1,78 @@
+package com.make.projects.security;
+
+import com.make.projects.entity.Users;
+import com.make.projects.entity.enumType.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.*;
+
+public class UserPrincipal implements OAuth2User, UserDetails {
+
+    private  Users user;
+    private Map<String,Object> attributes; //auth정보받을곳
+
+
+    public UserPrincipal(Users user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRoles().name();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
