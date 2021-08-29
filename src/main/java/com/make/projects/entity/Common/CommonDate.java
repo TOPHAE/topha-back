2e92d302fc17c5d4f6571d3f51bd7f5131c12a03
woojin126 <woojin126@naver.com>
2021-08-29 20:59:16 +0900

    공통 클래스 (등록시간, 수정시간)

---
+++ src/main/java/com/make/projects/entity/Common/CommonDate.java
@@ -0,0 +1,23 @@
+package com.make.projects.entity.Common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class CommonDate {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime modifiedDate;
+}
