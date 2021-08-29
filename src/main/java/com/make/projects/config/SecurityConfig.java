f063468d4c450715f0d4bba11bd3c75419ac30fe
woojin126 <woojin126@naver.com>
2021-08-29 20:56:28 +0900

    SecurityConfig 설정파일

---
+++ src/main/java/com/make/projects/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.make.projects.config;
+
+import com.make.projects.security.oauth2.CustomOauth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOauth2UserService customOauth2UserService;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//세션정책을 설정한다는것은 인증 처리 관점에서 스프링 시큐리티가 더 이상 세션쿠키 방식의 인증 메카니즘으로 인증처리를 하지 않겠다는 의미
+        //http.formLogin().disable();
+        //http.httpBasic().disable();
+
+
+        http.authorizeRequests()
+                .antMatchers("/api/**").access("hasRole('ROLE_USER')")
+                .antMatchers("/api/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll()
+            .and()
+                .formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
+            .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOauth2UserService);
+    }
+}
