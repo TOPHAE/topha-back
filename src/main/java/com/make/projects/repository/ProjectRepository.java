5f737ed82da3c48cf7f8157e2e2a11c50068af10
woojin126 <woojin126@naver.com>
2021-09-03 05:19:22 +0900

    ProjectRepository

--- src/main/java/com/make/projects/repository/ProjectRepository.java
+++ src/main/java/com/make/projects/repository/ProjectRepository.java
@@ -1,6 +1,6 @@
 package com.make.projects.repository;
 
-import com.make.projects.entity.Project;
+import com.make.projects.model.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {
