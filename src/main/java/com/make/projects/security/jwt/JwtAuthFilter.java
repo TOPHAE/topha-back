a85c1bc9b27b16eb9ef6a37a0bcca8123478e17e
woojin126 <woojin126@naver.com>
2021-09-03 05:17:08 +0900

    요청과 함께온 jwt 토큰을 검증하는곳

---
+++ src/main/java/com/make/projects/security/jwt/JwtAuthFilter.java
@@ -0,0 +1,68 @@
+package com.make.projects.security.jwt;
+
+import com.make.projects.exception.OAuth2AuthenticationEx;
+import com.make.projects.service.CustomUserDetailsService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Slf4j
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private JwtUtil jwtUtil;
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public JwtAuthFilter(JwtUtil jwtUtil, CustomUserDetailsService userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    public JwtAuthFilter(){
+
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = getJwtFromRequest(request);
+            System.out.println("토큰확인 = " + jwt);
+            if (StringUtils.hasText(jwt) && jwtUtil.validateToken(jwt)) {
+                String userId = jwtUtil.getUserIdFromToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userId);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception ex) {
+            throw new OAuth2AuthenticationEx("Could not set user authentication in security context", HttpStatus.BAD_REQUEST);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+}
