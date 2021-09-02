bfc759a80dd9ac1e4a1e1833b7a42f5594ae92d0
woojin126 <woojin126@naver.com>
2021-09-03 05:20:35 +0900

    Oauth Login 성공후 처리

---
+++ src/main/java/com/make/projects/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,99 @@
+package com.make.projects.security.oauth2;
+
+import com.make.projects.common.util.CookieUtils;
+import com.make.projects.config.ApplicationProperties;
+import com.make.projects.exception.OAuth2AuthenticationEx;
+import com.make.projects.security.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Optional;
+import static com.make.projects.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
+@Slf4j
+@Component
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+
+    private final JwtUtil jwtUtil;
+    private final ApplicationProperties applicationProperties;
+
+    @Autowired
+    public OAuth2AuthenticationSuccessHandler(JwtUtil jwtUtil, ApplicationProperties applicationProperties) {
+        this.jwtUtil = jwtUtil;
+        this.applicationProperties = applicationProperties;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        System.out.println("request1 = " + request);
+        System.out.println("authentication1 = " + authentication);
+        String targetUrl = determineTargetUrl(request, response,authentication);
+        System.out.println("타겟 = " + targetUrl);
+
+        if(response.isCommitted()){
+            log.debug("응답 완료되어 리다이렉트 할수 없는 Url" + targetUrl);
+            return;
+        }
+        clearAuthenticationAttributes(request, response);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+    /**
+     * SuccessHandler의 여러가지 메서드중 로그인 성공시 이동경로를 설정
+     */
+    @Override
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        Optional<String> redirectUri = CookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue);
+        System.out.println("타겟2 = " + redirectUri);
+
+        if(redirectUri.isPresent() && !isAuthorizedRedirectUri(redirectUri.get())){
+            throw new OAuth2AuthenticationEx("리다이렉트 URI 가없으면 인증 진행이 불가능합니다.", HttpStatus.BAD_REQUEST);
+        }
+
+
+        String targetUrl = redirectUri.orElse(getDefaultTargetUrl());
+
+        String token = jwtUtil.createToken(authentication);
+
+
+        return UriComponentsBuilder.fromUriString(targetUrl)
+                .queryParam("token", token)
+                .build().toUriString();
+    }
+    protected void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        HttpCookieOAuth2AuthorizationRequestRepository.removeAuthorizationRequestResponse(request, response);
+    }
+
+    private boolean isAuthorizedRedirectUri(String uri) {
+        URI clientRedirectUri = URI.create(uri);
+
+        System.out.println("클라유알엘 = " + clientRedirectUri);
+
+
+        //host와 port가 맞는지 다른지 검증 사용자들이 다른 경로를 사용할 수도 있음
+        return applicationProperties.getoAuth2().getAuthorizedRedirectUris()
+                .stream()
+                .anyMatch(authorizedRedirectUri -> {
+                    URI authorizedURI = URI.create(authorizedRedirectUri);
+                    if(authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+                            && authorizedURI.getPort() == clientRedirectUri.getPort()) {
+                        return true;
+                    }
+                    return false;
+                });
+    }
+}
