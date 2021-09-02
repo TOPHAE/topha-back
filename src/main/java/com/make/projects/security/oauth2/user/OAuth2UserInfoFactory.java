cd2ff374673d5754e264b89a39f98aa4f57c2c5f
woojin126 <woojin126@naver.com>
2021-09-03 05:20:19 +0900

    facebook -> github 변경

--- src/main/java/com/make/projects/security/oauth2/user/OAuth2UserInfoFactory.java
+++ src/main/java/com/make/projects/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -1,7 +1,8 @@
 package com.make.projects.security.oauth2.user;
 
-import com.make.projects.entity.enumType.Provider;
+import com.make.projects.model.domain.enumType.Provider;
 import com.make.projects.exception.OAuth2AuthenticationEx;
+import org.springframework.http.HttpStatus;
 
 import java.util.Map;
 
@@ -11,13 +12,13 @@ public class OAuth2UserInfoFactory {
 
         if(registrationId.equalsIgnoreCase(Provider.google.name())){
             return new GoogleOAuth2UserInfo(attributes);
-        }else if(registrationId.equalsIgnoreCase(Provider.facebook.name())) {
-            return new FacebookOAuth2UserInfo(attributes);
+        }else if(registrationId.equalsIgnoreCase(Provider.github.name())) {
+            return new GithubOAuth2UserInfo(attributes);
         }else if(registrationId.equalsIgnoreCase(Provider.kakao.name())){
             Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
             return new KakaoOAuth2UserInfo(kakao_account);
         }else{
-            throw new OAuth2AuthenticationEx("죄송합니다" + registrationId + "는지원하지 않습니다.");
+            throw new OAuth2AuthenticationEx("죄송합니다 아직 지원하지않는 소셜입니다.", HttpStatus.BAD_REQUEST);
         }
     }
 }
