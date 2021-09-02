9ef1fe7b34016827ad8904ab31b1265e295a6752
woojin126 <woojin126@naver.com>
2021-09-03 05:23:35 +0900

    일반 로그인시 필요한 서비스

---
+++ src/main/java/com/make/projects/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.make.projects.service;
+
+
+import com.make.projects.exception.OAuth2AuthenticationEx;
+import com.make.projects.model.domain.Users;
+import com.make.projects.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        Users user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+        );
+
+        return new CustomUserDetails(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        Users user = userRepository.findById(id).orElseThrow(
+            () -> new OAuth2AuthenticationEx("User not found ", HttpStatus.NOT_FOUND)
+        );
+
+        return new CustomUserDetails(user);
+    }
+}
