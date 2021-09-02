ab7d049c7b973f874c80197f1b36a7a180fcae46
woojin126 <woojin126@naver.com>
2021-09-03 05:18:56 +0900

    SecurityConfig

--- src/main/java/com/make/projects/config/SecurityConfig.java
+++ src/main/java/com/make/projects/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.make.projects.config;
 
-import com.make.projects.security.oauth2.CustomOauth2UserService;
+import com.make.projects.exception.Http401ErrorEntryPoint;
+import com.make.projects.security.jwt.JwtAuthFilter;
+import com.make.projects.security.oauth2.CustomOAuth2UserService;
+import com.make.projects.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import com.make.projects.security.oauth2.OAuth2AuthenticationFailureHandler;
+import com.make.projects.security.oauth2.OAuth2AuthenticationSuccessHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +18,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 /*https://github.com/keumtae-kim/spring-boot-react-blog*/
 @RequiredArgsConstructor
 @Configuration
@@ -21,42 +32,74 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
         prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomOauth2UserService customOauth2UserService;
+    private CustomOAuth2UserService customOauth2UserService;
+    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
-    @Override
+    @Autowired
+    public SecurityConfig(CustomOAuth2UserService customOauth2UserService, OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler, OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler){
+        this.customOauth2UserService = customOauth2UserService;
+        this.oAuth2AuthenticationFailureHandler = oAuth2AuthenticationFailureHandler;
+        this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
+    }
+
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.cors();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//세션정책을 설정한다는것은 인증 처리 관점에서 스프링 시큐리티가 더 이상 세션쿠키 방식의 인증 메카니즘으로 인증처리를 하지 않겠다는 의미
-        http.formLogin().disable();
-        http.httpBasic().disable();
-/* .and()
-                .formLogin()
-                .loginPage("/loginForm")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/")*/
-
-        http.authorizeRequests()
-                .antMatchers("/api/**").access("hasRole('ROLE_USER')")
+        http.
+                cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션정책을 설정한다는것은 인증 처리 관점에서 스프링 시큐리티가 더 이상 세션쿠키 방식의 인증 메카니즘으로 인증처리를 하지 않겠다는 의미
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(new Http401ErrorEntryPoint())
+                .and()
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/error",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .antMatchers(HttpMethod.GET,"/api/users/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/projects/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/api/comments/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/projects/**").access("hasRole('ROLE_USER')")
+                .antMatchers(HttpMethod.POST,"/api/comments/**").access("hasRole('ROLE_USER')")
                 .antMatchers("/api/admin/**").access("hasRole('ROLE_ADMIN')")
                 .anyRequest().permitAll()
             .and()
                 .oauth2Login()
                 .authorizationEndpoint()
                 .baseUri("/oauth2/authorize")
-                .and()
-                .redirectionEndpoint()
-                .baseUri("/oauth2/callback/*")
-                .and()
-                .userInfoEndpoint() //oauth2Login 성공 이후의 설정을 시작
-                .userService(customOauth2UserService);
-    }
-    /*   .authorizationEndpoint()
-                .baseUri("/oauth/authorize")
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository())
             .and()
                 .redirectionEndpoint()
                 .baseUri("/oauth2/callback/*")
-            .and()*/
+            .and()
+                .userInfoEndpoint()
+                .userService(customOauth2UserService)
+            .and()
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+                .failureHandler(oAuth2AuthenticationFailureHandler);
+
+    }
+    @Bean
+    public Http401ErrorEntryPoint http401ErrorEntryPoint() {
+        return new Http401ErrorEntryPoint();
+    }
+
+    @Bean
+    public JwtAuthFilter tokenAuthenticationFilter() {
+        return new JwtAuthFilter();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -68,4 +111,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    private AuthorizationRequestRepository<OAuth2AuthorizationRequest> cookieAuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
 }
+
