86eddae2e44a2d1058514394c4e07ed3bbc155ad
woojin126 <woojin126@naver.com>
2021-09-02 23:25:05 +0900

    기본 설정 hashcode,equals,toString

---
+++ src/main/java/com/make/projects/model/BaseModel.java
@@ -0,0 +1,21 @@
+package com.make.projects.model;
+
+import java.io.Serializable;
+
+public abstract class BaseModel implements Serializable {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
