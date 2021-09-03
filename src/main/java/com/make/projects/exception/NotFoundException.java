e8d41e0828345a7d39ce42e51c774511952c7103
woojin126 <woojin126@naver.com>
2021-09-04 06:28:45 +0900

    NotFoundException custom

--- src/main/java/com/make/projects/exception/NotFoundException.java
+++ src/main/java/com/make/projects/exception/NotFoundException.java
@@ -1,34 +1,23 @@
 package com.make.projects.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+public class NotFoundException extends RuntimeException{
 
-/**
- * @author Kim Keumtae
- */
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class NotFoundException extends  RuntimeException {
-    private String resourceName;
-    private String fieldName;
-    private Object fieldValue;
-
-    public NotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
+    public NotFoundException() {
     }
 
-    public String getResourceName() {
-        return resourceName;
+    public NotFoundException(String message) {
+        super(message);
     }
 
-    public String getFieldName() {
-        return fieldName;
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 
-    public Object getFieldValue() {
-        return fieldValue;
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
