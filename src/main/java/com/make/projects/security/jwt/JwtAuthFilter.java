a6910882dc13896c9ca485ad3b8f7ced53225e4e
woojin126 <woojin126@naver.com>
2021-09-04 06:22:41 +0900

    JwtAuthFilter 요청온 토큰 검증

--- src/main/java/com/make/projects/security/jwt/JwtAuthFilter.java
+++ src/main/java/com/make/projects/security/jwt/JwtAuthFilter.java
@@ -1,10 +1,7 @@
 package com.make.projects.security.jwt;
 
-import com.make.projects.exception.OAuth2AuthenticationEx;
+import com.make.projects.exception.authexception.OAuth2AuthenticationEx;
 import com.make.projects.service.CustomUserDetailsService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
