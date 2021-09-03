a238bd9f310058c0fbdeafe4544eba5287828369
woojin126 <woojin126@naver.com>
2021-09-04 06:29:48 +0900

    Project Controller 프로젝트 저장 PathVariable 추가 , 프로젝트 전체조회

--- src/main/java/com/make/projects/controller/ProjectApiController.java
+++ src/main/java/com/make/projects/controller/ProjectApiController.java
@@ -1,32 +1,36 @@
 package com.make.projects.controller;
 
 import com.make.projects.model.domain.Project;
+import com.make.projects.model.dto.ResponseDto;
 import com.make.projects.repository.ProjectRepository;
+import com.make.projects.service.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ProjectApiController {
 
     private final ProjectRepository projectRepository;
 
-
-    @PostMapping("/project/save")
-    public ResponseEntity<Project> projectSave(@RequestBody Project project){
+    @PostMapping("/project/save/{userId}")
+    public ResponseEntity<Project> projectSave(@RequestBody Project project, @PathVariable Long userId){
         projectRepository.save(project);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @GetMapping("/api/project/selectAll")
-    public ResponseEntity<List<Project>> projectSelect(){
-
-        return new ResponseEntity<>(projectRepository.findAll(),HttpStatus.OK);
+    @GetMapping("/project/selectAll")
+    public ResponseDto<List<Project>> projectSelect(){
+        List<Project> all = projectRepository.findAll();
+        return new ResponseDto<List<Project>>(all,HttpStatus.OK.value());
     }
 
 
