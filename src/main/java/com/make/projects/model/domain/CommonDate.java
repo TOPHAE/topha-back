dd23b834df63da8ac3e2884a839f32da45f32d46
woojin126 <woojin126@naver.com>
2021-09-04 06:24:27 +0900

    공통 등록시간, 수정시간

--- src/main/java/com/make/projects/model/domain/CommonDate.java
+++ src/main/java/com/make/projects/model/domain/CommonDate.java
@@ -1,6 +1,6 @@
 package com.make.projects.model.domain;
 
-import lombok.Getter;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -9,9 +9,9 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-@Getter
 public class CommonDate {
 
     @CreatedDate
