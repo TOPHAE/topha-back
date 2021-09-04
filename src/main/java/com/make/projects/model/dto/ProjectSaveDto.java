bb767051b47a9ab0c2f6188564f541fbd43f1dd9
woojin126 <woojin126@naver.com>
2021-09-04 20:39:58 +0900

    ProjectSaveDto

--- src/main/java/com/make/projects/model/dto/ProjectSaveDto.java
+++ src/main/java/com/make/projects/model/dto/ProjectSaveDto.java
@@ -1,13 +1,38 @@
 package com.make.projects.model.dto;
 
+import com.make.projects.model.domain.Project;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Set;
+
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProjectSaveDto {
 
+    private String title;
+    private String nickname;
+    private Integer likeCount;
+    private Integer viewCount;
+    private String userSpec;
+    private Set<String> tech;
+    private Set<String> spec;
+    private Long userId;
+    
 
+    public Project.ProjectBuilder toEntity(){
+
+       return Project.builder()
+                .nickname(nickname)
+                .spec(spec)
+                .userSpec(userSpec)
+                .tech(tech)
+                .viewCount(viewCount)
+                .likeCount(likeCount)
+                .title(title);
+    }
 }
