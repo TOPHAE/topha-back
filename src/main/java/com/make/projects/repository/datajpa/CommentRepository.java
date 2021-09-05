924b71bbd02aaededeea016dddbf96201c5f4bce
woojin126 <woojin126@naver.com>
2021-09-06 08:35:47 +0900

    CommentRepository

--- src/main/java/com/make/projects/repository/datajpa/CommentRepository.java
+++ src/main/java/com/make/projects/repository/datajpa/CommentRepository.java
@@ -1,4 +1,4 @@
-package com.make.projects.repository;
+package com.make.projects.repository.datajpa;
 
 import com.make.projects.model.domain.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
