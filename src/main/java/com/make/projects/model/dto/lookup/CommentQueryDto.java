a6ce3dd7d447fd5c3e5aa7140898a118338dce59
woojin126 <woojin126@naver.com>
2021-09-06 08:35:34 +0900

    CommentQueryDto

--- src/main/java/com/make/projects/model/dto/lookup/CommentQueryDto.java
+++ src/main/java/com/make/projects/model/dto/lookup/CommentQueryDto.java
@@ -1,15 +1,19 @@
 package com.make.projects.model.dto.lookup;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 public class CommentQueryDto {
-
-    private Long projectId;
+    private Long project_Id;
     private String content;
 
-    public CommentQueryDto(Long projectId, String content) {
-        this.projectId = projectId;
+
+    @QueryProjection
+    public CommentQueryDto(Long project_Id, String content) {
+        this.project_Id = project_Id;
         this.content = content;
     }
 }
+
