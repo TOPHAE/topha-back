b574283c0aefe386fb24ab616ba35590b11d129b
woojin126 <woojin126@naver.com>
2021-09-07 07:36:06 +0900

    Jwt 토큰으로 유저정보 조회 반환

---
+++ src/main/java/com/make/projects/model/dto/lookup/ResponseOAuthUser.java
@@ -0,0 +1,16 @@
+package com.make.projects.model.dto.lookup;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ResponseOAuthUser {
+    private Long userId;
+    private String nickname;
+    private String email;
+    private String roles;
+    private String provider;
+}
