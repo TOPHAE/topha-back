dc1284830edd8df0c7212caa71b596434f917cd9
woojin126 <woojin126@naver.com>
2021-09-08 23:57:13 +0900

    REquestUpdateUser룸복 기본생성자 추가

--- src/main/java/com/make/projects/model/dto/RequestUpdateUser.java
+++ src/main/java/com/make/projects/model/dto/RequestUpdateUser.java
@@ -3,16 +3,18 @@ package com.make.projects.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class RequestUpdateUser {
 
-    @NotBlank(message = "닉네임 필수 입력")
+
     private String nickname;
-    @NotBlank(message = "전문 분야 선택")
+
     private String tech;
+
 }
