b3fe5536b6b86b859779a50aebf229864beb7843
woojin126 <woojin126@naver.com>
2021-09-09 17:50:56 +0900

    JwtTokenException

---
+++ src/main/java/com/make/projects/exception/jwtexception/JwtTokenException.java
@@ -0,0 +1,32 @@
+package com.make.projects.exception.jwtexception;
+
+import org.springframework.http.HttpStatus;
+
+public class JwtTokenException extends RuntimeException{
+
+    HttpStatus httpStatus;
+
+    public JwtTokenException(HttpStatus httpStatus){
+        this.httpStatus = httpStatus;
+    }
+
+    public JwtTokenException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public JwtTokenException(String message, Throwable cause, HttpStatus httpStatus) {
+        super(message, cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public JwtTokenException(Throwable cause, HttpStatus httpStatus) {
+        super(cause);
+        this.httpStatus = httpStatus;
+    }
+
+    public JwtTokenException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, HttpStatus httpStatus) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.httpStatus = httpStatus;
+    }
+}
