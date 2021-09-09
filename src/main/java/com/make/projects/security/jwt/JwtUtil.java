15b7b6bd1ec603c5268a97a2814cfb1bac364213
woojin126 <woojin126@naver.com>
2021-09-09 14:20:21 +0900

    변경사항없음

--- src/main/java/com/make/projects/security/jwt/JwtUtil.java
+++ src/main/java/com/make/projects/security/jwt/JwtUtil.java
@@ -1,14 +1,14 @@
 package com.make.projects.security.jwt;
-import com.make.projects.config.ApplicationProperties;
 import com.make.projects.config.auth.CustomUserDetails;
+import com.make.projects.exception.authexception.JwtTokenException;
+import com.make.projects.exception.authexception.OAuth2AuthenticationEx;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
-
-import javax.validation.Valid;
 import java.util.Date;
 import java.util.stream.Collectors;
 
@@ -78,24 +78,16 @@ public class JwtUtil {
     }
 
     public boolean validateToken(String authToken) {
-        System.out.println("authToken = " + authToken);
+
         try{
-            System.out.println("시크릿키 = " + secretKey);
             Jwts.parser().setSigningKey(secretKey).parseClaimsJws(authToken);
             return true;
-        } catch (SignatureException e){
-            log.info("유효하지 않은 서명");
-        } catch (MalformedJwtException e){
-            log.info("토큰 구성 오류");
-        } catch (ExpiredJwtException e){
-            log.info("만료 기간 초과");
-        } catch (UnsupportedJwtException e){
-            log.info("기존의 형식과 다릅니다");
-        } catch (IllegalArgumentException e){
-            log.info("부적합한 인자");
+        } catch (SignatureException | MalformedJwtException | UnsupportedJwtException | ExpiredJwtException | IllegalArgumentException e){
+            throw new JwtTokenException(e.getMessage() ,e.getCause(), HttpStatus.FORBIDDEN);
+        } catch (Exception e){
+            throw new JwtTokenException(e.getMessage(), e.getCause(), HttpStatus.FORBIDDEN);
         }
 
-        return false;
     }
 
 }
