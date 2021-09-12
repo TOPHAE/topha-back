503a1c93c43b9d288c5ccaffe1d0b944f956897b
woojin126 <woojin126@naver.com>
2021-09-12 20:07:15 +0900

    import ProjectQueryDto 제거

--- src/main/java/com/make/projects/repository/datajpa/CustomProjectRepository.java
+++ src/main/java/com/make/projects/repository/datajpa/CustomProjectRepository.java
@@ -2,7 +2,6 @@ package com.make.projects.repository.datajpa;
 
 import com.make.projects.model.domain.Project;
 import com.make.projects.model.dto.lookup.CommentQueryDto;
-import com.make.projects.model.dto.lookup.ProjectQueryDto;
 
 import java.util.List;
 
