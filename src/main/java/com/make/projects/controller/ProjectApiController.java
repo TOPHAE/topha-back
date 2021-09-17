bcc58be39d80eaa5ac22bf6dbe6f75d071fbb863
woojin126 <woojin126@naver.com>
2021-09-17 12:37:29 +0900

    @PageableDefault 페이징 어노테이션 추가

--- src/main/java/com/make/projects/controller/ProjectApiController.java
+++ src/main/java/com/make/projects/controller/ProjectApiController.java
@@ -6,9 +6,13 @@ import com.make.projects.model.dto.ProjectSaveDto;
 import com.make.projects.model.dto.Result;
 import com.make.projects.model.dto.lookup.ProjectQueryDto;
 import com.make.projects.model.dto.lookup.ResponseProjectDto;
+import com.make.projects.repository.datajpa.CommentRepository;
 import com.make.projects.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +28,6 @@ public class ProjectApiController {
 
     private final ProjectService projectService;
 
-
     //프로젝트 게시물 단일조회
     @GetMapping("/project/selectOne/{projectId}")
     public Result<ProjectQueryDto> projectSelectOne(@PathVariable Long projectId){
@@ -36,8 +39,8 @@ public class ProjectApiController {
 
     //프로젝트 게시물 전체조회
     @GetMapping("/project/selectAll")
-    public Result<List<ProjectQueryDto>> projectSelectAll(){
-        List<ProjectQueryDto> projectQueryDtos = projectService.selectAll();
+    public Result<List<ProjectQueryDto>> projectSelectAll(@PageableDefault(size = 1) Pageable pageable){
+        List<ProjectQueryDto> projectQueryDtos = projectService.selectAll(pageable);
         return new Result<>(projectQueryDtos,HttpStatus.OK.value());
     }
 
