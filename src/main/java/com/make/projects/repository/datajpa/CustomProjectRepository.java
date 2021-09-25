610152f35cfe3329655473467461567d08966fad
woojin126 <woojin126@naver.com>
2021-09-25 13:44:31 +0900

    컬렉션 값타입이 있어서 new Projection 사용이 불가능, 그래서 그냥 fetch join 사용

--- src/main/java/com/make/projects/repository/datajpa/CustomProjectRepository.java
+++ src/main/java/com/make/projects/repository/datajpa/CustomProjectRepository.java
@@ -1,14 +1,12 @@
 package com.make.projects.repository.datajpa;
 
 import com.make.projects.model.domain.Project;
-import com.make.projects.model.dto.lookup.ProjectQueryDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface CustomProjectRepository {
 
     Project selectOneProject(Long projectId);
-    Page<ProjectQueryDto> selectAllProject(Pageable pageable);
+    Page<Project> selectAllProject(Pageable pageable);
 }
