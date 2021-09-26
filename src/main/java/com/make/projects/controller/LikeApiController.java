23e3b4ffcfe9ffb8dd77aeaf6dbf91362feae6e1
woojin126 <woojin126@naver.com>
2021-09-26 15:29:34 +0900

    좋아요 ApiController 좋아요 취소, 좋아요 등록, 좋아요 카운트

---
+++ src/main/java/com/make/projects/controller/LikeApiController.java
@@ -0,0 +1,67 @@
+package com.make.projects.controller;
+
+import com.make.projects.annotaions.LoginUser;
+import com.make.projects.config.auth.CustomUserDetails;
+import com.make.projects.model.domain.Users;
+import com.make.projects.repository.datajpa.UserRepository;
+import com.make.projects.service.LikeService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class LikeApiController {
+
+    private final LikeService likeService;
+    private final UserRepository userRepository;
+
+    @ApiOperation(value = "좋아요 카운트")
+    @GetMapping("/like/{projectId}")
+    public ResponseEntity<List<String>> getLikeCount(@PathVariable Long projectId,
+                                                     @LoginUser Users loginUser) throws Exception {
+        log.info("project-id : {}", projectId);
+        log.info("loginUser : {} ", loginUser);
+
+        List<String> resultData = likeService.count(projectId,loginUser);
+
+        log.info("likeCount : {} " , resultData);
+
+        return new ResponseEntity<>(resultData , HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "좋아요 취소")
+    @DeleteMapping("/like/{projectId}")
+    public ResponseEntity<String> cancelLike(@LoginUser Users loginUser,
+                                             @PathVariable Long projectId){
+        if(loginUser != null){
+            likeService.cancelLike(loginUser, projectId);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "좋아요 등록")
+    @PostMapping("/like/{projectId}")
+    public ResponseEntity<String> addLike(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @PathVariable Long projectId) {
+        boolean result = false;
+
+        if (customUserDetails.getUsername() != null) {
+            result = likeService.addLike(customUserDetails.getUser(), projectId);
+        }
+
+        return result ?
+                new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}
