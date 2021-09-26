4e95f6c1efaba067ee9d9e1c11b74a462faf0b96
woojin126 <woojin126@naver.com>
2021-09-26 15:30:14 +0900

    좋아요 JPARepository

---
+++ src/main/java/com/make/projects/repository/datajpa/LikeRepository.java
@@ -0,0 +1,15 @@
+package com.make.projects.repository.datajpa;
+
+import com.make.projects.model.domain.Like;
+import com.make.projects.model.domain.Project;
+import com.make.projects.model.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like,Long> {
+
+    Optional<Integer> countByProject(Project project);
+    //유저와, 프로젝트 인자로 받아 해당 게시물에, 해당 회원이 좋아요를 등록한적이 있는지 체크하는 용도
+    Optional<Like> findByUsersAndProject(Users users , Project project);
+}
