1e43aece1dc7ea75370f12274d0505672c17d681
woojin126 <woojin126@naver.com>
2021-12-10 23:50:09 +0900

    UserRepository

--- src/main/java/com/make/projects/repository/datajpa/user/UserRepository.java
+++ src/main/java/com/make/projects/repository/datajpa/user/UserRepository.java
@@ -1,10 +1,10 @@
 package com.make.projects.repository.datajpa.user;
 
-import com.make.projects.model.domain.Users;
+import com.make.projects.model.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Users,Long> {
-    Optional<Users> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
 }
